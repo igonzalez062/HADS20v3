c4e02d2813c726dc868bfc73e2f3978e559c9eb9
igonzalez062 <ikergonzalez86@gmail.com>
2017-03-21 19:58:30 +0100

    Version1
    Version1

---
+++ WebApp2017HADS/WebApp2017HADS/Inicio.aspx.vb
@@ -0,0 +1,30 @@
+﻿Public Class Inicio
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub BotonLogin_Click(sender As Object, e As EventArgs) Handles BotonLogin.Click
+        Dim resultado As String
+
+        resultado = LibreriaFunciones.Funciones.loginUsuario(TextBoxEmail.Text, TextBoxContraseña.Text)
+        If resultado = "P" Then
+            Session("UserID") = TextBoxEmail.Text
+            Response.Redirect("./Profesor/Profesor.aspx")
+        ElseIf resultado = "A" Then
+            Session("UserID") = TextBoxEmail.Text
+            Response.Redirect("./Alumno/Alumno.aspx")
+        ElseIf resultado = "ERRORDATOSINCORRECTOS" Then
+            LabelError.Text = "DATOS INCORRECTOS"
+        ElseIf resultado = "ERRORNOUSER" Then
+            LabelError.Text = "NO EXISTE ESE USUARIO"
+        ElseIf resultado = "ERRORAPERTURADECONEXION" Then
+            LabelError.Text = "ERROR EN LA APERTURA DE LA CONEXION"
+        End If
+    End Sub
+
+    Protected Sub LinkButtonOlvidadoContraseña_Click(sender As Object, e As EventArgs) Handles LinkButtonOlvidadoContraseña.Click
+
+    End Sub
+End Class
---
+++ WebApp2017HADS/WebApp2017HADS/Registro.aspx.vb
@@ -0,0 +1,24 @@
+﻿Public Class Registro
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub ButtonRegistrar_Click(sender As Object, e As EventArgs) Handles ButtonRegistrar.Click
+        Dim resultado As String
+
+        resultado = LibreriaFunciones.Funciones.insertarUsuario(TextBoxEmail.Text, TextBoxNombre.Text, TextBoxApellidos.Text, TextBoxDNI.Text, TextBoxContraseña.Text, TextBoxPreguntaSecreta.Text, TextBoxRespuestaSecreta.Text)
+
+        If resultado = "OK" Then
+            Response.Redirect("./ResultadoRegistroHecho.aspx?state=ok")
+        Else
+            Response.Redirect("./ResultadoRegistroHecho.aspx?state=error")
+        End If
+    End Sub
+
+    Protected Sub ButtonVolver_Click(sender As Object, e As EventArgs) Handles ButtonVolver.Click
+        Response.Redirect("./Inicio.aspx")
+    End Sub
+
+End Class
